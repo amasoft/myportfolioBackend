0b83d68f1f75279e2d67f2629ec8c49d53c8b01a
amasoft <amadifaraday@gmail.com>
2023-06-16 04:45:51 +0100

    implemented email sending

--- routers/contactmeRouter.js
+++ routers/contactmeRouter.js
@@ -1,33 +1,34 @@
 const router = require("express").Router();
 const contactmodel = require("../models/ContactmeModel");
-
+const sendmail = require("../utili/sendmail");
 router.get("/", (req, res) => {
   res.send("welcome from router folder");
 });
 
 router.post("/contactme", async (req, res) => {
-  const contactDetails = await new contactmodel({
-    name: req.body.name,
-    email: req.body.email,
-    messages: req.body.messages,
-    project: req.body.project,
+  // const contactDetails = await new contactmodel({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   messages: req.body.messages,
+  //   project: req.body.project,
 
-    // name: "Amadi",
-    // email: "amadimarcelino",
-    // messages: "work needded",
-    // project: "web app",
-  }).save();
-  if (contactDetails) {
-    res.status(200).json({
-      status: 200,
-      message: "Data inserted succesfully",
-      data: contactDetails,
-    });
-  } else {
-    res.json({
-      message: "erro adding record",
-    });
-  }
+  //   // name: "Amadi",
+  //   // email: "amadimarcelino",
+  //   // messages: "work needded",
+  //   // project: "web app",
+  // }).save();
+  // if (contactDetails) {
+  //   res.status(200).json({
+  //     status: 200,
+  //     message: "Data inserted succesfully",
+  //     data: contactDetails,
+  //   });
+  // } else {
+  //   res.json({
+  //     message: "erro adding record",
+  //   });
+  // }
   // res.send("contact succesfully added")
+  sendmail(req.body.name, req.body.project, req.body.messages, req.body.email);
 });
 module.exports = router;
---
+++ utili/sendmail.js
@@ -0,0 +1,34 @@
+var nodemailer = require("nodemailer");
+const sendmail = (name, projectType, projectDesc, email) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "amadifaraday@gmail.com",
+      pass: "foympeqrkqmkukac",
+    },
+  });
+
+  var mailOptions = {
+    from: "amadifaraday@gmail.com",
+    to: "amadiarinzechukwu@gmail.com",
+    subject: "PortfolioC(Hire Me):Backend Service",
+    text: "you have a new request for backend task ",
+    html:
+      "<h4>welcome </h4>" +
+      `<p> Dear Patrick  ${name}  request your backend service</p>` +
+      `<p>below is the details :</p>` +
+      `<p>Type of Project :${projectType}</p>` +
+      `<p>project Description :${projectDesc}</p>` +
+      `<p>Contact email :${email}</p>`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+module.exports = sendmail;
